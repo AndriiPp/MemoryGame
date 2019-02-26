c1791e96b6dba17ecd0fcb28b0a9ee2ec603b212
AndriiPp <andriipp@MacBook-Pro-Evgen.local>
2019-02-26 15:29:14 +0200

    Initial Commit

---
+++ Memory Game/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Memory Game
+//
+//  Created by AndriiPp on 2/26/19.
+//  Copyright © 2019 AndriiPp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
